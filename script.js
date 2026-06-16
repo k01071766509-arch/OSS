9b804ab75941cd45d121f1daa7e253f3700040fd
k01071766509-arch <k01071766509@gmail.com>
2026-06-16 17:28:24 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function showMessage() {
+  const message = document.getElementById("message");
+
+  message.innerText =
+    "핵심 암기: Main=배포, Develop=통합, Feature=기능 개발, Release=배포 전 테스트, Hotfix=긴급 수정";
+}
